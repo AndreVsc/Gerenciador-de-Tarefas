ec57d9eaaf5a7548631e2b68289ca3ad5bb882cf
AndreVsc <vsandre40@gmail.com>
2023-10-06 23:57:23 -0300

    0.0.1.4v
    Começando responsividade do site

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import NavbarEstoque from "../components/navbar/NavbarEstoque";
+import Waves from "../assets/waves-removebg.png"
 function App() {
   return(
     <>
