079d80781d5a134fbb4d1af8b183d5e39baf5af9
AndreVsc <vsandre40@gmail.com>
2023-10-27 04:40:47 -0300

    0.0.3.7v

--- src/components/LinhaTempo/TimeLine.jsx
+++ src/components/LinhaTempo/TimeLine.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
 import './TimeLine.css';
 
 function TimeLine() {
   return (
     <div className='containerTime'>
         <div className='containerRecent'>
-
+          <p><strong>tipo</strong></p>
+          <p><strong>data</strong></p>
+          <p><strong>produto</strong></p>
+          <p><strong>original</strong></p>
+          <p><strong>alteração</strong></p>
+        </div>
+        <div className='containerLeg'>
+          <div className='pLeg'><p><div className='greenLed'></div>adição</p></div>
+          <div className='pLeg'><p><div className='redLed'></div>remoção</p></div>
+          <div className='pLeg'><p><div className='blueLed'></div>criação</p></div>
         </div>
         <div className='containerHist'>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
+          <input type="text" className='inputHist' disabled/>
+          <input type="text" className='inputHist' disabled/>
+          <input type="text" className='inputHist' disabled/>
+          <input type="text" className='inputHist' disabled/>
+          <input type="text" className='inputHist' disabled/>
+          <input type="text" className='inputHist' disabled/>
+          <input type="text" className='inputHist' disabled/>
         </div>
     </div>
   );
